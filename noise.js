10f89b01212468567c63c560d2c560115b0faa56
laurenfb <laurenfriesbrink@gmail.com>
2016-11-21 16:43:39 -0800

    this does not work

--- noise.js
+++ noise.js
@@ -3,6 +3,8 @@ $(document).ready( function() {
 
   var notes = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g']
 
+  var alphabet = "CCGGAAGFFEEDDDDCGGFEEDGGGFEEDCCGGAAGFFEEDDC".toLowerCase();
+
   var stop = function(note) {
     // stolen from jeannie who stole it from the internet
     var song = document.getElementById(note + 'Audio');
@@ -26,8 +28,28 @@ $(document).ready( function() {
     });
   }
 
+  var playNoteForFunsies = function(note) {
+    stop(note);
+    document.getElementById(note + 'Audio').play();
+
+  }
+
+  var playSong = function(nameOfSong) {
+    $('.alphabet').click( function() {
+      var interval = 500;
+      for (var i = 0; i < nameOfSong.length; i++) {
+        setInterval(playNoteForFunsies(nameOfSong[i]), interval);
+        interval += 500;
+        console.log(nameOfSong[i]);
+      }
+    });
+  }
+
   for (var i = 0; i < notes.length; i++) {
     playNoteClick(notes[i]);
     playNoteKeyPress(notes[i])
   };
+
+  playSong(alphabet);
+
 });
